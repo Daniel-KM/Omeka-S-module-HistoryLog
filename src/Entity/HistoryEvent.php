40bbe1332f744b52150402ea34a095a3c2468769
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-08-28 00:00:00 +0000

    Cleaned entity.

--- src/Entity/HistoryEvent.php
+++ src/Entity/HistoryEvent.php
@@ -132,7 +132,7 @@ class HistoryEvent extends AbstractEntity
      *      type="datetime",
      *      nullable=false,
      *      options={
-     *          "default":"CURRENT_TIMESTAMP"
+     *          "default": "NOW()"
      *      }
      * )
      */
